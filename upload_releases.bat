73ce61023716dee91484fa5519a8468dc3ef3ed9
Jack Dunn <jack.dunn.nz@gmail.com>
2014-09-26 19:10:21 -0400

    Add upload script for releases

---
+++ upload_releases.bat
@@ -0,0 +1,10 @@
+:: Uploads all zip files in the Releases directory to Sourceforge
+:: Call using: upload_releases.bat <sourceforge username>
+:: Requires 'pscp.exe' on the system path, installed with PuTTY
+
+set start=pscp
+set user=%1
+set end=%user%@frs.sourceforge.net:/home/frs/project/opensolver
+
+:: Upload each zip file in releases
+for /R %%G in (*.zip) do %start% "%%G" %end%
