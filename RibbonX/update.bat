7f01e6726d66271b031e6df4a106d414de0f6ed2
Jack Dunn <jack.dunn.nz@gmail.com>
2015-09-12 16:35:47 -0400

    Allow for updating RibbonX UI programmatically. Also allows it to be version controlled

---
+++ RibbonX/update.bat
@@ -0,0 +1,2 @@
+:: This should be run from the RibbonX folder
+zip -r ..\OpenSolver.xlam customUI _rels "[Content_Types].xml" -S -x .DS_Store
---
+++ RibbonX/update.sh
@@ -0,0 +1,2 @@
+# This should be run from the RibbonX folder
+zip -r ../OpenSolver.xlam customUI _rels "[Content_Types].xml" -x .DS_Store
