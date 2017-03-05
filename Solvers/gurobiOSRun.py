9dc8e807fef35a74c48c0bf226827a815f0dbd9d
Jack Dunn <jack.dunn.nz@gmail.com>
2017-03-05 12:26:33 -0500

    Change gurobi interface to use a separate file for status to work with Gurobi 7. Fixes #269

--- Solvers/gurobiOSRun.py
+++ Solvers/gurobiOSRun.py
@@ -13,15 +13,17 @@ except Exception as e:
 parser = argparse.ArgumentParser()
 parser.add_argument("modelfile", help="Path to the model.lp file")
 parser.add_argument("mipstartfile", help="Path to any mipstart .mst file")
-parser.add_argument("solutionfile", help="Path to write the solution file")
-parser.add_argument("sensitivityfile", 
+parser.add_argument("statusfile", help="Path to write the status file")
+parser.add_argument("solutionfile", help="Path to write the solution .sol file")
+parser.add_argument("sensitivityfile",
                     help="Path to write the sensitivity data")
-parser.add_argument("params", help="Additional Gurobi parameters to set", 
+parser.add_argument("params", help="Additional Gurobi parameters to set",
                     nargs="*")
 
 args = parser.parse_args()
 mod_path = args.modelfile
 mst_path = args.mipstartfile
+status_path = args.statusfile
 sol_path = args.solutionfile
 sense_path = args.sensitivityfile
 params_list = args.params
@@ -37,37 +39,37 @@ for param in params_list:
     try:
         m.setParam(name, float(value))
     except GurobiError as e:
-        with open(sol_path, 'w') as sol_file:
-            sol_file.write('Gurobi Error: %s' % e.message)
+        with open(status_path, 'w') as status_file:
+            status_file.write('Gurobi Error: %s' % e.message)
         sys.exit()
 
 # Catch any GurobiError that occurs when solving
 try:
     m.optimize()
 except GurobiError as e:
-    with open(sol_path, 'w') as sol_file:
-        sol_file.write('Gurobi Error: %s' % e.message)
+    with open(status_path, 'w') as status_file:
+        status_file.write('Gurobi Error: %s' % e.message)
     sys.exit()
 
-with open(sol_path, 'w') as sol_file:
-    sol_file.write(str(m.status)+ '\n')
+with open(status_path, 'w') as status_file:
+    status_file.write(str(m.status) + '\n')
 
-    if not m.status in frozenset((3, 4, 5)):
-        m.write(sol_path)
-        vars = m.getVars()
-        cons = m.getConstrs()
-        with open(sense_path, 'w') as sense_file:
-            try:
-                vals = [map(str, m.getAttr(t, vars))
-                        for t in ['RC', 'SAObjLow', 'SAObjUp']]
-                for k in zip(*vals):
-                    sense_file.write(','.join(k) + '\n')
-            except:
-                pass
-            try:
-                vals = [map(str, m.getAttr(t, cons))
-                        for t in ['Pi', 'RHS', 'Slack', 'SARHSLow', 'SARHSUp']]
-                for k in zip(*vals):
-                    sense_file.write(','.join(k) + '\n')
-            except:
-                pass
+if not m.status in frozenset((3, 4, 5)):
+    m.write(sol_path)
+    vars = m.getVars()
+    cons = m.getConstrs()
+    with open(sense_path, 'w') as sense_file:
+        try:
+            vals = [map(str, m.getAttr(t, vars))
+                    for t in ['RC', 'SAObjLow', 'SAObjUp']]
+            for k in zip(*vals):
+                sense_file.write(','.join(k) + '\n')
+        except:
+            pass
+        try:
+            vals = [map(str, m.getAttr(t, cons))
+                    for t in ['Pi', 'RHS', 'Slack', 'SARHSLow', 'SARHSUp']]
+            for k in zip(*vals):
+                sense_file.write(','.join(k) + '\n')
+        except:
+            pass
